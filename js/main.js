c8a7e1918c82b297ecc0dd20a5dae477bc8f56d7
Dhaval Prajapati <dhavalprajapati1396@gmail.com>
2018-11-06 11:30:18 -0500

    change code

--- js/main.js
+++ js/main.js
@@ -1,83 +1,86 @@
-document.addEventListener("DOMContentLoaded",init);
+document.addEventListener("DOMContentLoaded", init);
 
-function getRandomInteger (min, max) {
-    min = Math.ceil(min); 
-    max = Math.floor(max + 1); 
-    return Math.floor(Math.random() * (max - min)) + min; 
+//function getRandomInteger(min, max) {
+//    min = Math.ceil(min);
+//    max = Math.floor(max + 1);
+//    return Math.floor(Math.random() * (max - min)) + min;
+//}
+//let pages = [];
+
+function init() {
+//console.log("hi");
+    /* pages = document.querySelectorAll(".page");
+     console.log(pages);
+     document.getElementById("btnSend").addEventListener("click", function () {
+         pages[0].classList.toggle("active");
+         pages[1].classList.toggle("active");
+     });
+     document.getElementById("btnBack").addEventListener("click", function () {
+         pages[0].classList.toggle("active");
+         pages[1].classList.toggle("active");
+     });*/
+
+    document.getElementById("btnSend").addEventListener("click", getData);
+  //  document.getElementById("btnBack").addEventListener("click",dataBack());
+
+
 }
 
-function init(){
-    
-    
-    document.getElementById("btnSend").addEventListener("click",getData,serverData);
-    
-}
-function getData(){
-    
-    let url="https://davidst.edumedia.ca/mad9014/nums.php?digits=6&max=42";
-    
+
+let returnNum;
+
+function getData() {
+
+    let url = "https://davidst.edumedia.ca/mad9014/nums.php";
+
+
     let formData = new FormData();
+
+    formData.append("digits", document.getElementById("digits").value);
+    formData.append("max", document.getElementById("max").value);
     
-    formData.append("digits",document.getElementById("digits").value);
-    formData.append("max",document.getElementById("max").value);
+   // console.log(formData);
     
-    let customSettings={
-        mode:"cors",
-        method:"POST",
-        body : formData
+    
+    let customSettings = {
+        mode: "cors",
+        method: "POST",
+        body: formData
     };
+
+    let request = new Request(url, customSettings);
     
-    let request=new Request(url,customSettings);
+    console.log(customSettings);
     
-     fetch(request)
-    .then(function(response){
-        console.log(response);
-        getRandomInteger(response.value);
-        return response.json();
-    })
-    .then(function(data){
-        console.log(data);
-    })
-    .catch(function(error){
-        alert(error);
-    });
-}
-function serverData(){
-    let url="https://davidst.edumedia.ca/mad9014/nums.php?digits=6&max=42";
-    let customSettings={
-        mode:"cors",
-        method:"GET"
-    };
-    
-    let request=new Request(url,customSettings);
-    
+    console.log(request);
     fetch(request)
-    .then(function(response){
-        //console.log(response);
-        return response.json();
-    })
-    .then(function(data){
-        let ul=document.querySelector('.num_list');
-        ul.innerHTML="";
-        for(let item in data){
-            let li=document.createElement("li");
-            li.innerHTML=data[item];
-            ul.appendChild(li);
-        }
+        .then(function (response){
+            console.log(response);
+           // getRandomInteger(response.value);
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
         
-        /*let df=new DocumentFragment();
-        jsondata.forEach((user)=>{
-            let li=document.createElement('li');
-            let ph=document.createElement('placeholder');
-            ph.textContent=
-            
-        })*/
-    })
-    .catch(function(error){
-        alert(error);
-    });
+            returnNum = data.numbers;
+            let ul = document.querySelector('.num_list');
+            console.log(returnNum);
+            returnNum.forEach(function (num) {
+                let li = document.createElement("li");
+                li.innerHTML = num;
+                ul.appendChild(li);
+                li.style.listStyleType = "none";
+                li.style.border = "solid 0.1rem red";
+                li.style.padding = "2rem";
+                li.style.margin = "1rem";
+                console.log(num);
+            });
+        })
+        .catch(function (error) {
+            alert(error);
+        });
 }
-function navigate(){
-    
-    
-}
+//function dataBack(){
+//    
+//    formData.removeChild();
+//}
